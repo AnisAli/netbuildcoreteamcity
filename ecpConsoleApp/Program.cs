96b1e3f68675f79f0ea94a356b1682a509137c1b
Anis <anis.ali@smith.co>
2017-08-31 18:46:03 -0400

    initial code\

---
+++ ecpConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ecpConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test t = new test();
+
+            Console.WriteLine("Hello World! Test Branch" + t.ConvertString("Anis"));
+        }
+    }
+}
---
+++ ecpConsoleApp/test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ecpConsoleApp
+{
+  public  class test
+    {
+
+        public string ConvertString(string s)
+        {
+            return s + " converted";
+        }
+    }
+}
